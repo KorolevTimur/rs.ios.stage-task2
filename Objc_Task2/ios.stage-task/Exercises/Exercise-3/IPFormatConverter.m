dc21a360b1a485bf87d280d6a57f534a2d5fc657
Timur Korolev <timurkorolev89@gmail.com>
2021-05-17 20:01:02 +0300

    feat: solution of all tasks

--- Objc_Task2/ios.stage-task/Exercises/Exercise-3/IPFormatConverter.m
+++ Objc_Task2/ios.stage-task/Exercises/Exercise-3/IPFormatConverter.m
@@ -3,7 +3,32 @@
 @implementation IPFormatConverter
 
 - (NSString *)ipFormatConverter:(NSArray *)numbersArray {
-    return nil;
+    
+    NSString *outOfRangeException = @"The numbers in the input array can be in the range from 0 to 255.";
+    NSString *negativeNumberException = @"Negative numbers are not valid for input.";
+    NSMutableString *result = [[NSMutableString alloc] initWithString:@""];
+    NSMutableArray *formattedArray = [[NSMutableArray alloc] initWithArray:numbersArray];
+    
+    
+    if (numbersArray == nil || [numbersArray isEqual:@[]]) {
+        return  result;
+    }
+    if (numbersArray.count < 4) {
+        while (formattedArray.count < 4) {
+            [formattedArray addObject:@0];
+        }
+    }
+    
+    for (int i = 0; i < 4; i++) {
+        if ([formattedArray[i] intValue] < 0) {
+            return negativeNumberException;
+        } else if ([formattedArray[i] intValue] > 255) {
+            return outOfRangeException;
+        } else {
+            [result appendFormat:@"%@.", formattedArray[i]];
+        }
+    }
+    return [result substringToIndex:[result length] - 1];
 }
 
 @end
--- Objc_Task2/ios.stage-task/Exercises/Exercise-4/TwoDimensionalArraySorter.m
+++ Objc_Task2/ios.stage-task/Exercises/Exercise-4/TwoDimensionalArraySorter.m
@@ -3,7 +3,31 @@
 @implementation TwoDimensionalArraySorter
 
 - (NSArray *)twoDimensionalSort:(NSArray<NSArray *> *)array {
-    return @[];
+    
+    NSMutableArray<NSNumber *> *numbersArray = [NSMutableArray new];
+    NSMutableArray<NSString *> *stringsArray = [NSMutableArray new];
+    
+    if (![array[0] isKindOfClass: [NSArray class]]) {
+        return @[];
+    }
+    for (NSArray *subarray in array) {
+        for (id item in subarray) {
+            if ([item isKindOfClass: NSString.class]) {
+                [stringsArray addObject:item];
+            } else if ([item isKindOfClass: NSNumber.class]) {
+                [numbersArray addObject:item];
+            }
+        }
+    }
+    if (numbersArray.count == 0) {
+        return [stringsArray sortedArrayUsingSelector:@selector(localizedCaseInsensitiveCompare:)];
+    } else if (stringsArray.count == 0) {
+        return [numbersArray sortedArrayUsingSelector:@selector(compare:)];
+    } else {
+        
+        return [NSArray arrayWithObjects:[numbersArray sortedArrayUsingSelector:@selector(compare:)],
+                [[[stringsArray sortedArrayUsingSelector:@selector(localizedCaseInsensitiveCompare:)] reverseObjectEnumerator] allObjects], nil];
+    }
 }
 
 @end
